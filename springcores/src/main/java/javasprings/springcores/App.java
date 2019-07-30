8c2dfefb651296ccda4a1e3283465176cda52ff7
Sandeep Balivada <sandeepbalivada123@gmail.com>
2019-07-30 16:54:16 +0530

    new new

---
+++ springcores/src/main/java/javasprings/springcores/App.java
@@ -0,0 +1,13 @@
+package javasprings.springcores;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ springcores/src/main/java/javasprings/springcores/EmployeeApp.java
@@ -0,0 +1,10 @@
+package javasprings.springcores;
+
+public class EmployeeApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

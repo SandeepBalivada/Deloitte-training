f945472422b6b97d624622b0075a2a7080c761e4
Sandeep Balivada <sandeepbalivada123@gmail.com>
2019-08-05 17:05:53 +0530

    new commit again on mavens

---
+++ springmvcbootweb/src/main/java/boot/SampleController.java
@@ -0,0 +1,15 @@
+package boot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SampleController {
+	@RequestMapping("welcome")
+	public String welcome()
+	{
+		System.out.println("printing something");
+		return "Welcome";
+	}
+
+}
